2a081da31f64040990cf78e6a9235702835a196d
VladWorldWide27 <vld.pitt27@gmail.com>
2026-04-06 13:55:29 -0400

    todos completed

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,13 +1,20 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
+    private static String barista = "Vladimir-Coffeemeister"; // STUDENT_TODO_1A: Change name
     // STUDENT_TODO_1B: Add a nickname or title for the barista
 
     private static int cups = 0;
 
     public static void brew(String drink) {
-        // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
-        // STUDENT_TODO_2B: Add a second brew-related improvement
+        cups++;
+        System.out.println(barista + " is brewing: " + drink);
+
+        if (drink.equals("Espresso")) {
+            System.out.println("Strong espresso shot! Here you go.");
+        } 
+        else {
+            System.out.println("Smooth and creamy drink, Enjoy");
+        }
     }
 
     public static void printSummary() {
